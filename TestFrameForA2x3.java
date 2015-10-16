4ae218e2da0a92ba2468905bbe84eb67e132edc6
Michael Schaefers <Michael.Schaefers@HAW-Hamburg.de>
2015-10-16 09:05:23 +0200

    simplifying code

--- TestFrameForA2x3.java
+++ TestFrameForA2x3.java
@@ -2,26 +2,10 @@
 /**
  * A2x3: Fir - see task
  * 
- * @author  (your name(s)) 
- * @version (a version number or a date)
- */
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Wir erinnern uns: Niemals verschiedene Spachen in einem Programm mixen.
- * Oder konkreter: Entweder Variablen und Kommentar in deutsch oder in englisch.
- * 
- * Auch ist diese Art von Kommentar KEIN guter Kommentar.
- * Wir nutzen den Kommentar hier fuer Erklaerungen und Ausfuehrungen zur
- * Aufgabenstellung. Es  gilt "hier" die Regel:
- *  o) "Kommentar" der Kommentar ist in englisch.
- *  o) "Kommentar" der die Aufgabenstellung vertieft in deutsch.
- * Im Rahmen der Aufgabenstellung muessen Sie GUTEN Kommentar schreiben.
+ * @author   Michael Schäfers ; P1@Hamburg-UAS.eu 
+ * @version  2015/10/16
  */
 public class TestFrameForA2x3 {
-    
-    // Moegliche Exemplarvariablen bzw. Objekt-spezifische Variablen
-    // ... ???
-    
-    
     
     /**
      * print fir
@@ -29,13 +13,11 @@ public class TestFrameForA2x3 {
      * @param height height of fir
      */
     public void printFir( int height ){
-        // Ersetzen Sie diesen Kommentar durch Ihren Code.
-        // Geben Sie die letzen drei Zahlen aus.
-        // ...
-        for( int h=0; h<height; h++ ){
-            for( int x=1; x<height-h; x++ )  System.out.print( "." );
-            for( int x=0; x<2*h+1;    x++ )  System.out.print( "*" );
-            for( int x=1; x<height-h; x++ )  System.out.print( "." );
+        // Vermutlich ist "ab 1" für Anfänger angenehmer - "ab 0" ist aber ausdrücklich auch ok - wenn alles stimmt ;-)
+        for( int h=1; h<=height; h++ ){                                         // HIER gehört eine Abzählschleife (for) hin
+            for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehört eine Abzählschleife (for) hin
+            for( int x=1; x<=2*h-1;    x++ )  System.out.print( "*" );          // HIER gehört eine Abzählschleife (for) hin
+            for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehört eine Abzählschleife (for) hin
             System.out.println();
         }//for
     }//method()
