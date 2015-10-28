1cca26a8820a52904be790930bcb931053b6604e
Michael Schaefers <Michael.Schaefers@HAW-Hamburg.de>
2015-10-28 21:08:44 +0100

    WS15/16 1st release

--- TestFrameForA2x3.java
+++ TestFrameForA2x3.java
@@ -3,7 +3,7 @@
  * A2x3: Fir - see task
  * 
  * @author   Michael Schäfers ; P1@Hamburg-UAS.eu 
- * @version  2015/10/16
+ * @version  2015/10/28
  */
 public class TestFrameForA2x3 {
     
@@ -15,9 +15,21 @@ public class TestFrameForA2x3 {
     public void printFir( int height ){
         // Vermutlich ist "ab 1" für Anfänger angenehmer - "ab 0" ist aber ausdrücklich auch ok - wenn alles stimmt ;-)
         for( int h=1; h<=height; h++ ){                                         // HIER gehört eine Abzählschleife (for) hin
+            // 1. Variante
             for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehört eine Abzählschleife (for) hin
             for( int x=1; x<=2*h-1;    x++ )  System.out.print( "*" );          // HIER gehört eine Abzählschleife (for) hin
             for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehört eine Abzählschleife (for) hin
+            
+            /*
+            // 2.Variante
+            for( int x=1; x<=2*height-1; x++ ){
+                if( height-h<x  &&  x<height+h ){
+                    System.out.print( "*" );
+                }else{
+                    System.out.print( "." );
+                }//if
+            }//for
+            */
             System.out.println();
         }//for
     }//method()
