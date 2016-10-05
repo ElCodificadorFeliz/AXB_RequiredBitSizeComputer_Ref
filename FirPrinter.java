bf9516eedb6daee441e985a85074400c9dfba556
Michael Schaefers <Michael.Schaefers@HAW-Hamburg.de>
2016-10-05 13:22:19 +0200

    for safety's sake

--- FirPrinter.java
+++ FirPrinter.java
@@ -13,12 +13,12 @@ public class FirPrinter {
      * @param height height of fir
      */
     public void printFir( int height ){
-        // Vermutlich ist "ab 1" für Anfänger angenehmer - "ab 0" ist aber ausdrücklich auch ok - wenn alles stimmt ;-)
-        for( int h=1; h<=height; h++ ){                                         // HIER gehört eine Abzählschleife (for) hin
+        // Vermutlich ist "ab 1" fuer Anfaenger angenehmer - "ab 0" ist aber ausdruecklich auch ok - wenn alles stimmt ;-)
+        for( int h=1; h<=height; h++ ){                                         // HIER gehoert eine Abzählschleife (for) hin
             // 1. Variante
-            for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehört eine Abzählschleife (for) hin
-            for( int x=1; x<=2*h-1;    x++ )  System.out.print( "*" );          // HIER gehört eine Abzählschleife (for) hin
-            for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehört eine Abzählschleife (for) hin
+            for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehoert eine Abzählschleife (for) hin
+            for( int x=1; x<=2*h-1;    x++ )  System.out.print( "*" );          // HIER gehoert eine Abzählschleife (for) hin
+            for( int x=1; x<=height-h; x++ )  System.out.print( "." );          // HIER gehoert eine Abzählschleife (for) hin
             
             /*
             // 2.Variante
